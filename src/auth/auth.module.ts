4d20173baa47e2bcaee6e92154abe640155a0016
Darya Vedernikova <smmcream@gmail.com>
2026-07-03 13:08:01 +0300

    feat: jwt to auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      secret: process.env.JWT_ACCESS_SECRET ?? 'default-secret',
+      signOptions: {
+        expiresIn: process.env.JWT_ACCESS_EXPIRES_IN ?? '15m',
+      },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
