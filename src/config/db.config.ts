14feff0fa9aa42f1358ab384652cb73318bff071
Darya Vedernikova <smmcream@gmail.com>
2026-07-05 00:40:11 +0300

    fix config conflicts

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,14 +1,17 @@
 import { registerAs } from '@nestjs/config';
 import { DataSourceOptions } from 'typeorm';
 
-export const dbConfig = registerAs('DB_CONFIG', (): DataSourceOptions => ({
-  type: 'postgres',
+export const dbConfig = registerAs(
+  'DB_CONFIG',
+  (): DataSourceOptions => ({
+    type: 'postgres',
 
-  host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 5432,
-  username: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'skillswap_db',
-}));
+    host: process.env.DB_HOST || 'localhost',
+    port: Number(process.env.DB_PORT) || 5432,
+    username: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'skillswap_db',
+  }),
+);
 
 export type TDbConfig = ReturnType<typeof dbConfig>;
