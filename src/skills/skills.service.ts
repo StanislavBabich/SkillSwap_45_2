ab79eb7d3b0f3fbfc2021c017e21a7c038efa323
Darya Vedernikova <smmcream@gmail.com>
2026-07-09 21:06:56 +0300

    feat: add skills entity and resource

---
+++ src/skills/skills.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class SkillsService {
+  create() {
+    return 'This action adds a new skill';
+  }
+
+  findAll() {
+    return `This action returns all skills`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} skill`;
+  }
+
+  update(id: number) {
+    return `This action updates a #${id} skill`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} skill`;
+  }
+}
